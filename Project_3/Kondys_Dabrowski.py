b97ea50d18394adac00d9d8d86148277150f24e4
MarceloTrapano <kacper7866@gmail.com>
2025-05-06 11:43:36 +0200

    Optimizing player

--- Project_3/Kondys_Dabrowski.py
+++ Project_3/Kondys_Dabrowski.py
@@ -2,24 +2,26 @@ import numpy as np
 from player import Player
 
 class Kondys_Dabrowski(Player):
-    """Gracz w oszusta, którego strategia opiera się o liczbę kart w ręce oraz o figurę karty przeciwnika. 
+        """Gracz w oszusta, którego strategia opiera się o liczbę kart w ręce oraz o figurę karty przeciwnika. 
     Macierz wypłat zmienia się w trakcie zadania przez, co dostosowywana jest taktyka pod nieznaną macierz wypłat.
     Gracz operuje na prawdopodobieństwach odnoszących się do wykonywania odpowiednich akcji. Strategią dominującą
     jest zagranie najniższej karty w momencie gdy możemy ją zagrać. Jeśli nasza najmniejsza karta ma figurę niższą
     od figury na stole gracz ma możliwość oszukania. Oszukanie w tym momencie to deklaracja najbliższej karty z ręki
     i postawienie karty najmniejszej. Gracz ma też możliwość nie oszukania i wystawienia karty najbliższej tej na stole.
-    Jeśli nie ma karty do oszukania to albo dobiera, albo deklaruje losową wyższą lub równą kartę zadeklarowanej.
+    Jeśli nie ma karty do oszukania to deklaruje losową wyższą lub równą kartę zadeklarowanej.
     Dodatkowo zaimplementowano strategię, która dla dużej liczby kart w ręce sprawia, że gracz wybiera wyższe karty, aby
     potem zmuszać przeciwnika do ciągnięcia kart, lub oszukiwania.
     """
     def __init__(self, name: str) -> None:
         self.name: str = name
         self.cards: list[tuple[int, int]] = [] # edycja konstruktora klasy w celu pozbycia się komunikatów w IDE.
+        self.seen: list[tuple[int, int]] = [] # dodanie tablicy z widzianymi kartami
     def lowestCardDeclaired(self, declared_card: tuple[int, int]) -> tuple[int, int]:
+        
         """Funkcja do wybierania karty o wartości najbliższej karcie zadeklarowanej.
 
         Args:
-            declared_card (tuple(int, int)): Zadeklarowana karta.
+            declared_card (tuple[int, int]): Zadeklarowana karta.
         
         Returns:
             tuple[int, int]: Najbliższa karta z ręki do zadeklarowanej.
@@ -41,7 +43,9 @@ class Kondys_Dabrowski(Player):
         """Określa decyzję gracza o wystawieniu karty poprawnej, oszukaniu lub ciągnięciu kart. Decyzja jest oparta na
         liczebności kart w ręce gracza. Strategia przyjmuje większe prawdopodobieństwo oszustwa dla małej liczby kart.
         Dodatkowo jeśli nasz gracz ma dużo kart to przyjmuje strategię wystawiania dużych wartości figur do zmuszania
-        przeciwnika do ciągnięcia kart.
+        przeciwnika do ciągnięcia kart lub oszustwa. Dobieranie kart rozpatrujemy tylko wtedy kiedy zostaje nam jedna karta.
+        Dobieranie jest strategią ściśle zdominowaną, ponieważ jak oszukamy to w najgorszym wypadku skończymy z jedną kartą,
+        mniej niż przy dobieraniu.
 
         Args:
             declared_card (tuple[int,int] | None): Karta na stole.
@@ -54,8 +58,11 @@ class Kondys_Dabrowski(Player):
         idx: int
         choiche: tuple[tuple[int,int], tuple[int, int]]
         minimal_card: tuple[int, int]
-        fake_card: tuple[int, int]|int
+        fake_card: tuple[int, int]
         num_of_cards: int = len(self.cards)
+
+        self.seen.extend(self.cards)
+        self.seen = list(set(self.seen)) # kompresja listy widzianych kart
 
         # strategia zakładająca wystawianie wysokich kart w momencie, gdy mamy dużo kart
         if num_of_cards > 10:
@@ -82,7 +89,15 @@ class Kondys_Dabrowski(Player):
         fake_card = self.lowestCardDeclaired(declared_card)
         if fake_card[0] == -1: # jeśli nie jesteśmy w stanie wybrać karty do oszustwa, wybieramy losowo
             if num_of_cards != 1: # nie możemy oszukać ostatniej karty
-               return np.random.choice(np.array([(minimal_card, (np.random.randint(declared_card[0], 15), np.random.randint(0,4))), "draw"], dtype = object), p = [0.8, 0.2])
+               # dobieranie kart jest strategią ściśle zdominowaną
+               random_card = np.random.randint(declared_card[0], 15), np.random.randint(0,4)
+               i = 0
+               while random_card in self.seen: # jeśli losowa karta była widziana powtarza losowanie
+                   random_card = np.random.randint(declared_card[0], 15), np.random.randint(0,4)
+                   if i == 1000: # wszystkie karty widziane, brak możliwości bezkarnego oszukania
+                       break
+                   i += 1
+               return minimal_card, random_card
             return 'draw'
         
         idx = np.random.choice([0,1], p = [prob, 1-prob])
@@ -100,6 +115,7 @@ class Kondys_Dabrowski(Player):
         Returns:
             bool: Decyzja o sprawdzeniu czy przeciwnik oszukał (True - oszukał, False - nie oszukał).
         """
+        
         prob: float = 0
         if opponent_declaration in self.cards: 
             return True # sprawdzenie czy zadeklarowana karta przeciwnika jest w twojej ręce
